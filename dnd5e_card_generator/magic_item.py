827c547132672773d10d600c0dec6583da1d30e1
Balthazar Rouberol <br@imap.cc>
2024-04-17 18:12:56 +0200

    MagicItem refactoring, to make it clearer how the main content is generated

--- dnd5e_card_generator/magic_item.py
+++ dnd5e_card_generator/magic_item.py
@@ -19,6 +19,10 @@ class MagicItem:
     image_url: str
     recharges: int
 
+    def highlight_die_value(self, text) -> str:
+        die_value_pattern = r"\dd\d+ " + damage_type_text(self.lang)
+        return re.sub(die_value_pattern, lambda match: f"<b>{match.group(0)}</b>", text)
+
     @property
     def icon(self):
         return self.type.icon
@@ -35,10 +39,6 @@ class MagicItem:
     def rarity_text(self) -> str:
         return self.rarity.translate(self.lang)
 
-    def highlight_die_value(self, text) -> str:
-        die_value_pattern = r"\dd\d+ " + damage_type_text(self.lang)
-        return re.sub(die_value_pattern, lambda match: f"<b>{match.group(0)}</b>", text)
-
     @property
     def subtitle(self) -> str:
         subtitle = f"{self.type_text}, {self.rarity_text}"
@@ -46,27 +46,37 @@ class MagicItem:
             subtitle += f" {self.attunement_text}"
         return subtitle
 
+    @property
+    def recharges_text(self) -> list[str]:
+        if not self.recharges:
+            return []
+        return ["fill | ", f"boxes | {self.recharges} | 1.5"]  # 1.5 is in em
+
+    @property
+    def subtitle_text(self) -> list[str]:
+        return [
+            f"subtitle | {self.subtitle}",
+            "rule",
+        ]
+
+    @property
+    def item_text(self) -> list[str]:
+        return [
+            f"text | {self.highlight_die_value(text_part)}" for text_part in self.text
+        ]
+
+    @property
+    def contents_text(self) -> list[str]:
+        return self.subtitle_text + self.text + self.recharges_text
+
     def to_card(self) -> dict:
-        extra_content = []
-        if self.recharges:
-            extra_content.extend(
-                ["fill | ", f"boxes | {self.recharges} | 1.5"]
-            )  # 1.5 is in em
         card = {
             "count": 1,
             "color": self.color,
             "title": self.title,
             "icon": self.icon,
             "icon_back": self.icon,
-            "contents": [
-                f"subtitle | {self.subtitle}",
-                "rule",
-            ]
-            + [
-                f"text | {self.highlight_die_value(text_part)}"
-                for text_part in self.text
-            ]
-            + extra_content,
+            "contents": self.contents_text,
         }
         if self.image_url:
             card["background_image"] = self.image_url
